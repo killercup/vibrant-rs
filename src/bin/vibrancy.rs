26b18ca56b2d1741631839ce7d98f1d6050c4391
Pascal Hertleif <killercup@gmail.com>
2015-09-18 10:46:02 +0200

    Don't Output Image Dimensions

--- src/bin/vibrancy.rs
+++ src/bin/vibrancy.rs
@@ -4,7 +4,6 @@ extern crate vibrant;
 use std::env;
 use std::path::Path;
 
-use image::GenericImage;
 use vibrant::Vibrancy;
 
 fn main() {
@@ -12,8 +11,6 @@ fn main() {
         .expect("No source image given.");
     let img = image::open(&Path::new(&source))
         .ok().expect(&format!("Could not load image {:?}", source));
-
-    println!("{:?}", img.dimensions());
 
     println!("{}", Vibrancy::new(&img));
 }
