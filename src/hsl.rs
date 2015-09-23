948260e0fde2825261e72dcd8c22626fcd1a76be
Pascal Hertleif <killercup@gmail.com>
2015-09-23 17:58:49 +0200

    Refactor HSL Test Macro, Add HSL to RGB Tests

--- src/hsl.rs
+++ src/hsl.rs
@@ -128,30 +128,55 @@ fn hue_to_rgb(p: f64, q: f64, t: f64) -> f64 {
 #[cfg(test)]
 mod test {
     macro_rules! test_rgb_to_hsl {
-        (($r:expr, $g:expr, $b:expr) <=> ($h:expr, $s:expr, $l:expr)) => {
-            {
+        ($name:ident, ($r:expr, $g:expr, $b:expr) <=> ($h:expr, $s:expr, $l:expr)) => {
+            #[test]
+            fn $name() {
                 // Round gracefully to half a percent
                 const EPSILON: f64 = 0.05;
                 // Round gracefully to half a degree
                 const EPSILON_DEGREE: f64 = 0.5;
+                // Round gracefully for RGB bytes
+                const EPSILON_RGB: i32 = 2;
 
                 let rgb = Rgb::from_channels($r as u8, $g as u8, $b as u8, 0_u8);
                 let hsl = HSL::from_pixel(&rgb);
 
                 assert!(
-                    $h - hsl.h <= EPSILON_DEGREE,
-                    "Converting {:?} to HSL: H differs too much. Expected {}, got {}.",
-                        rgb, $h, hsl.h
+                    ($h - hsl.h).abs() <= EPSILON_DEGREE,
+                    "Converting {:?} to HSL: H differs too much. Expected {:?}, got {}. ({:?})",
+                        rgb, $h, hsl.h, hsl
                 );
                 assert!(
-                    $s - hsl.s <= EPSILON,
-                    "Converting {:?} to HSL: S differs too much. Expected {}, got {}.",
-                        rgb, $s, hsl.s
+                    ($s - hsl.s).abs() <= EPSILON,
+                    "Converting {:?} to HSL: S differs too much. Expected {}, got {}. ({:?})",
+                        rgb, $s, hsl.s, hsl
                 );
                 assert!(
-                    $l - hsl.l <= EPSILON,
-                    "Converting {:?} to HSL: L differs too much. Expected {}, got {}.",
-                        rgb, $l, hsl.l
+                    ($l - hsl.l).abs() <= EPSILON,
+                    "Converting {:?} to HSL: L differs too much. Expected {}, got {}. ({:?})",
+                        rgb, $l, hsl.l, hsl
+                );
+
+                // ...and back!
+                let hsl = HSL { h: $h, s: $s, l: $l };
+                let rgb = hsl.to_rgb().channels4();
+                let (r, g, b, _) = rgb;
+                let expectation = ($r, $g, $b, 255);
+
+                assert!(
+                    ($r as i32 - r as i32).abs() <= EPSILON_RGB,
+                    "Converting {:?} to RGB: R differs too much. Expected {:?}, got {:?}.",
+                        hsl, expectation, rgb
+                );
+                assert!(
+                    ($g as i32 - g as i32).abs() <= EPSILON_RGB,
+                    "Converting {:?} to RGB: G differs too much. Expected {:?}, got {:?}.",
+                        hsl, expectation, rgb
+                );
+                assert!(
+                    ($b as i32 - b as i32).abs() <= EPSILON_RGB,
+                    "Converting {:?} to RGB: B differs too much. Expected {:?}, got {:?}.",
+                        hsl, expectation, rgb
                 );
             }
         };
@@ -160,30 +185,27 @@ mod test {
     use image::{Rgb, Pixel};
     use super::HSL;
 
-    #[test]
-    fn rgb_to_hsl() {
-        // black
-        test_rgb_to_hsl!( (0, 0, 0) <=> (0_f64, 0_f64, 0_f64) );
+    // black
+    test_rgb_to_hsl!(black, (0, 0, 0) <=> (0_f64, 0_f64, 0_f64) );
 
-        // white
-        test_rgb_to_hsl!( (255, 255, 255) <=> (0_f64, 0_f64, 1_f64) );
+    // white
+    test_rgb_to_hsl!(white, (255, 255, 255) <=> (0_f64, 0_f64, 1_f64) );
 
-        // http://rgb.to/rgb/18,35,67
-        test_rgb_to_hsl!( (18, 35, 67) <=> (219_f64, 0.58_f64, 0.17_f64) );
+    // http://rgb.to/rgb/18,35,67
+    test_rgb_to_hsl!(blue, (18, 35, 67) <=> (219_f64, 0.58_f64, 0.17_f64) );
 
-        // http://rgb.to/hex/93c6cd
-        test_rgb_to_hsl!( (147, 198, 205) <=> (187_f64, 0.37_f64, 0.69_f64) );
+    // http://rgb.to/hex/93c6cd
+    test_rgb_to_hsl!(lightblue, (147, 198, 205) <=> (187_f64, 0.37_f64, 0.69_f64) );
 
-        // http://rgb.to/hex/bada55
-        test_rgb_to_hsl!( (186, 218, 85) <=> (74_f64, 0.64_f64, 0.59_f64) );
+    // http://rgb.to/hex/bada55
+    test_rgb_to_hsl!(bada55, (186, 218, 85) <=> (74_f64, 0.64_f64, 0.59_f64) );
 
-        // http://rgb.to/hex/ff0
-        test_rgb_to_hsl!( (255, 255, 0) <=> (60_f64, 1_f64, 0.5_f64) );
+    // http://rgb.to/hex/ff0
+    test_rgb_to_hsl!(yellow, (255, 255, 0) <=> (60_f64, 1_f64, 0.5_f64) );
 
-        // http://rgb.to/rgb/198,250,172
-        test_rgb_to_hsl!( (198, 250, 172) <=> (100_f64, 0.89_f64, 0.83_f64) );
+    // http://rgb.to/rgb/198,250,172
+    test_rgb_to_hsl!(lightgreen, (198, 250, 172) <=> (100_f64, 0.89_f64, 0.83_f64) );
 
-        // http://rgb.to/hex/faadc7
-        test_rgb_to_hsl!( (250, 173, 199) <=> (340_f64, 0.89_f64, 0.83_f64) );
-    }
+    // http://rgb.to/hex/faadc7
+    test_rgb_to_hsl!(lightpink, (250, 173, 199) <=> (340_f64, 0.89_f64, 0.83_f64) );
 }
